331236040470e2f600ad8cb7f65b19e3bbb6cff7
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-24 12:15:49 +0100

    project completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../questions';
-import quizCompleteImg from '../assets/quiz-complete.png';
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 function Quiz() {
 
@@ -23,12 +23,7 @@ function Quiz() {
     );
 
     if(quizIsComplete){
-        return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy icon"/>
-                <h2>Quiz Completed</h2>
-            </div>
-        )
+        return <Summary userAnswers={userAnswers}/>
     }
 
     return (
